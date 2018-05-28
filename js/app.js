548b3db18e5518d73ee818a2f99739b37a8d9b6c
Jesray Garciano <jesray.garciano@gmail.com>
2018-05-28 11:56:44 +0800

    Modifying JS

--- js/app.js
+++ js/app.js
@@ -5,3 +5,14 @@ const empties = document.querySelectorAll('.empty');
 fill.addEventListener('dragstart',dragStart);
 fill.addEventListener('dragend', dragEnd);
 
+// Drag Functions
+function dragStart() {
+    // console.log('Start');
+    this.className += ' hold';
+    setTimeout(() => this.className = 'invisible', 0);
+    
+}
+
+function dragEnd() {
+    console.log('End');
+}
