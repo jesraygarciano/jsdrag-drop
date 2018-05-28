f878af54018860158a802fd8ea5d6a7b8ccc5252
Jesray Garciano <jesray.garciano@gmail.com>
2018-05-28 13:49:54 +0800

    Finishing the scripts

--- js/app.js
+++ js/app.js
@@ -5,6 +5,14 @@ const empties = document.querySelectorAll('.empty');
 fill.addEventListener('dragstart',dragStart);
 fill.addEventListener('dragend', dragEnd);
 
+// Loop through empties and call drag Events
+for(const empty of empties) {
+    empty.addEventListener('dragover', dragOver);
+    empty.addEventListener('dragenter', dragEnter);
+    empty.addEventListener('dragleave', dragLeave);
+    empty.addEventListener('drop', dragDrop);
+}
+
 // Drag Functions
 function dragStart() {
     // console.log('Start');
@@ -14,5 +22,25 @@ function dragStart() {
 }
 
 function dragEnd() {
-    console.log('End');
+    // console.log('End');
+    this.className = 'fill';
+}
+
+function dragOver(e){
+    // console.log('Over');
+    e.preventDefault();
+}
+function dragEnter(e){
+    // console.log('Enter');    
+    e.preventDefault();
+    this.className += ' hovered';
+}
+function dragLeave(){
+    // console.log('Leave');
+    this.className = 'empty';
+}
+function dragDrop(){
+    // console.log('Drop');
+    this.className = 'empty';
+    this.append(fill);
 }
