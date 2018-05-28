9745d2504dfd069d6f7d7b69b735e7195a305b2e
Jesray Garciano <jesray.garciano@gmail.com>
2018-05-28 11:36:50 +0800

    Modifying JS

--- js/app.js
+++ js/app.js
@@ -1,2 +1,7 @@
 const fill = document.querySelector('.fill');
 const empties = document.querySelectorAll('.empty');
+
+// Fill Listeners
+fill.addEventListener('dragstart',dragStart);
+fill.addEventListener('dragend', dragEnd);
+
